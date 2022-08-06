0030c23841f240b337a0a9aaab5a5c441a8a7cf3
Ravidev01 <ravichandran052015@gmail.com>
2022-08-06 22:47:26 +0530

    blogs features

--- src/app/pages/BlogPage/BlogPage.jsx
+++ src/app/pages/BlogPage/BlogPage.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import BlogCard from './components/BlogCard';
+import { blogCardData } from '../../../data';
 
 function BlogPage() {
   return (
-    <div>BlogPage</div>
+    <>
+      <h2 style={{ marginLeft: '30px' }}>Blogs</h2>
+      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '5px' }}>
+        {blogCardData.map((item) => (
+          <BlogCard key={item.id} data={item} />
+        ))}
+      </div>
+    </>
   );
 }
 
